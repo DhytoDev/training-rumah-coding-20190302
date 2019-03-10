cbfe1fa91f9f08ae14f7b0dda0ca888ff9d57baf
Fordyta Abubakar <dhytodev@gmail.com>
2019-03-10 14:25:53 +0800

    show patients data to listview

--- app/src/main/java/id/ac/unhas/rs/pasienku/activity/MainActivity.java
+++ app/src/main/java/id/ac/unhas/rs/pasienku/activity/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
             @Override
             public void onClick(View view) {
                 addPatient();
+                finish();
             }
         });
 
--- app/src/main/java/id/ac/unhas/rs/pasienku/activity/PatientListActivity.java
+++ app/src/main/java/id/ac/unhas/rs/pasienku/activity/PatientListActivity.java
@@ -1,8 +1,12 @@
 package id.ac.unhas.rs.pasienku.activity;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.List;
@@ -28,8 +32,33 @@ public class PatientListActivity extends AppCompatActivity {
         patientListView = findViewById(R.id.list_view_patient);
         searchView = findViewById(R.id.search_view);
 
-        getPatientList();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPatientList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.menu_add_patient) {
+            Intent intent = new Intent(PatientListActivity.this,
+                    MainActivity.class);
+            startActivity(intent);
+        } else if(id == R.id.menu_delete_all) {
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void getPatientList() {
